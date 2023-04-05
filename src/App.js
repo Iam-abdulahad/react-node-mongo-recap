13637d4ea99dfd397d22e30272fabb2972f6e6fe
Abdul Ahad <ahmmedsumon087@gmai.com>
2023-04-05 06:37:52 +0600

    created a from for add user to server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleAddUser = event => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+
+    const user = { name, email };
+    console.log(user);
+
+    // fetch('http://localhost:5000/user', {
+    //   method: 'POST',
+    //   headers: {
+    //     'content-type': 'application/json'
+    //   },
+    //   body: JSON.stringify(user)
+    // })
+
+    //   .then(res => res.json())
+    //   .then(data => {
+    //     console.log("success:", data)
+    //   })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Please add a user</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name='name' placeholder='Name' required />
+        <br />
+        <input type="email" name='email' placeholder='Email' required />
+        <br />
+        <input type="submit" value="submit" />
+      </form>
     </div>
   );
 }
